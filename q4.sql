da55a5594e0a00b296ff154687342e258c89255e
shay-tam <102288891+shay-tam@users.noreply.github.com>
2022-04-05 09:48:04 +0300

    fix

--- q4.sql
+++ q4.sql
@@ -1,6 +1,7 @@
 select distinct name, year
 from (authors natural join conferences ) T
-where subarea="vision" and exists (select name
-                                    from (authors natural join conferences) S
-                                    where T.year=S.year and S.subarea="ml")
+where institution='Hebrew University of Jerusalem' and subarea='vision' and exists (select name
+                                                                                    from (authors natural join conferences) S
+                                                                                    where institution='Hebrew University of Jerusalem' and
+                                                                                          T.name=S.name and T.year=S.year and S.subarea='ml')
 order by name, year;
