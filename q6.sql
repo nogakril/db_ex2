ce075c99e725e56dce04edcf28d28da4c200e585
shay-tam <102288891+shay-tam@users.noreply.github.com>
2022-04-05 09:49:37 +0300

    fix

--- q6.sql
+++ q6.sql
@@ -1,9 +1,8 @@
-select name
+select distinct name
 from (authors natural join conferences) C
-where not exsists(select name
+where not exists(select name
                   from (authors natural join conferences) T
-                  where T.name="Noam Nisan" and T.area="ai" and not in (select name
-                                                                        from (authors natural join conferences) S
-                                                                        where S.area="ai" and S.name=C.name))
-)
+                  where T.name='Noam Nisan' and T.area='ai' and T.conference not in (select conference
+                                                                                     from (authors natural join conferences) S
+                                                                                     where S.area='ai' and S.name=C.name))
 order by name;
