1f4275228e316b25acff8fefad1581a900e69373
shay-tam <102288891+shay-tam@users.noreply.github.com>
2022-04-05 09:46:57 +0300

    Fixed country name + adjustedcount inequation

--- q3.sql
+++ q3.sql
@@ -1,4 +1,4 @@
 select distinct institution, name
 from authors natural join institutions natural join conferences
-where country="Israel" and adjustedcount>1 and (area="ai" or subarea="db")
+where country='il' and adjustedcount>=2 and (area='ai' or subarea='db')
 order by institution, name;
